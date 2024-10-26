58c66c6ed1398e58ab414ab55bfd0d16e987be7c
reyzalhildha <reyzal@adiwisista.com>
2024-10-26 21:34:11 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+
+function Home() {
+    return (
+        <>
+            <div id="home" className="container">
+                <p style={{fontSize: '80px'}}>
+                    HOME
+                </p>
+            </div>
+            <div id="about" className="container">
+                <p style={{fontSize: '80px'}}>
+                    ABOUT
+                </p>
+            </div>
+            <div id="history" className="container">
+                <p style={{fontSize: '80px'}}>
+                    HISTORY
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default Home;
---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,16 @@
+
+function Navbar() {
+    return (
+        <>
+            <div className="container-navbar"> 
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#history">History</a></li>
+                </ul>
+            </div>
+        </>
+    );
+}
+
+export default Navbar;
